79257d04b1fcab7a328c631f45a10dcbf2c14df8
allt2709 <renatotorrescalle@gmail.com>
2021-09-25 20:21:21 -0500

    gitignore data

--- todoApp/settings.py
+++ todoApp/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vomwfzwa3^a62v!wx71ggq-4g@6#eyw(gnc@f-^rjg0jgxj38k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
